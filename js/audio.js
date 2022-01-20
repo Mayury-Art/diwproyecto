a377677f759774d08623d99ae1eb1257815ae9c2
Mayury-Art <m.arteagaromero20@gmail.com>
2022-01-20 12:03:16 +0100

    fotos despliegue

--- js/audio.js
+++ js/audio.js
@@ -1,18 +1,25 @@
 function accionPlay() {
-    var reproducir = new Audio();
-    reproducir.src = "./media/audio1.mp3";
-    reproducir.play();
+    let audios = ["./media/audio1.mp3",
+        "./media/audio2.mp3",
+        "./media/audio3.mp3",
+        "./media/audio4.mp3",
+        "./media/audio5.mp3"];
+    for (let i = 0; i < 5; i++) {
+        var i = new Audio();
+        i.src = audios[i];
+        i.play();
+    }
 }
 
 
 
 function iniciar() {
     button = document.querySelectorAll("button");
-    button[0].addEventListener("click", accionPlay, false);
-    
-    /* for (let i = 0; i < button.lenght; i++) {
+    //button[0].addEventListener("click", accionPlay, false);
+
+    for (let i = 0; i < button.lenght; i++) {
         button[i].addEventListener("click", accionPlay, false);
-    } */
+    }
 }
 
 window.addEventListener("load", iniciar, false);
